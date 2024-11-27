8ee962305ab657bb6f6ba63107b7dc603fdb06b0
eveningwater <854806732@qq.com>
2024-11-27 06:37:20 +0000

    feat:优化了editor组件

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -8,32 +8,33 @@ export interface MonacoEditorComponentProps<T> extends Omit<EditorProps, 'value'
   onChange?: (value: T) => void;
   options?: editor.IStandaloneEditorConstructionOptions;
 }
+const defaultOption = {
+  minimap: {
+    enabled: false,
+  },
+  scrollBeyondLastLine: false,
+  scrollbar: {
+    handleMouseWheel: true,
+  },
+  automaticLayout: true,
+};
 const MonacoEditorComponent = <T extends string>({
   value,
   onChange,
-  options = {
-    minimap: {
-      enabled: false,
-    },
-    scrollBeyondLastLine: false,
-    scrollbar: {
-      handleMouseWheel: false,
-    },
-    automaticLayout: true,
-  },
+  options = {},
   ...rest
 }: MonacoEditorComponentProps<T>) => {
   const [text, setText] = useState<T>();
   const editorRef = useRef<editor.IStandaloneCodeEditor>();
   useEffect(() => {
-     if(isString(value) && value){
-        setText(value)
-     }
-  },[value])
+    if (isString(value) && value) {
+      setText(value);
+    }
+  }, [value]);
 
   const handleEditorDidMount = (e: editor.IStandaloneCodeEditor) => {
     editorRef.current = e;
-  }
+  };
   return (
     <>
       {/* @ts-ignore */}
@@ -44,7 +45,7 @@ const MonacoEditorComponent = <T extends string>({
           onChange?.(value);
         }}
         value={text}
-        options={options}
+        options={{ ...defaultOption, ...options }}
         onMount={handleEditorDidMount}
         {...rest}
       />
